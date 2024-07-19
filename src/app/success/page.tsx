2a5a61dfa3558cbd52b5fa5ff000caa54148d7b9
CarlosLonghi <carlos.longhi@digitalone.com.br>
2024-07-19 15:42:26 -0300

    style: change style page success

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -45,37 +45,36 @@ export default async function Success({ searchParams }: { searchParams: { sessio
   const product = session.line_items.data[0].price.product;
 
   return (
-    <main className="flex flex-col gap-10 items-center justify-center flex-1">
-      <h1 className="text-4xl font-bold">Compra Realizada</h1>
+    <main className="flex flex-col items-center justify-center flex-1">
+      <h1 className="text-4xl font-bold mb-6">Compra Realizada</h1>
 
-      <div className="flex flex-col gap-6 items-center">
-        <div className="w-full max-w-xs h-96 p-2 rounded-md bg-gradient-to-b from-background to-secondary/50">
-          <Image src={product.images[0]} alt={product.name} width={255} height={530} className="object-contain h-full w-full rounded-md"/>
+      <div className="flex flex-col gap-6 items-center max-w-screen-lg p-6 bg-gradient-to-b from-background to-secondary/50 rounded-lg shadow-lg md:flex-row">
+        <div className="w-full h-96 p-2 rounded-lg overflow-hidden md:w-1/3">
+          <Image src={product.images[0]} alt={product.name} width={255} height={530} className="object-contain h-full w-full"/>
         </div>
 
-        <p className="text-2xl font-bold text-center">
-          {customerName} agradeçemos pela sua confiança! Seu pedido já está sendo preparado.
-        </p>
+        <div className="flex-1 space-y-12 text-center md:text-left">
+          <div className="space-y-4">
+            <h2 className="text-3xl font-bold">
+              Obrigado, {customerName}!
+            </h2>
+            <p className="text-2xl">
+              Seu pedido está sendo preparado, agradecemos pela sua confiança.😁
+            </p>
+          </div>
 
-        <table className="table-auto w-full max-w-xs">
-          <tbody>
-            <tr className="text-xl">
-              <td>Cliente:</td>
-              <td><strong>{customerName}</strong></td>
-            </tr>
-            <tr className="text-xl">
-              <td>Produto:</td>
-              <td><strong>{product.name}</strong></td>
-            </tr>
-          </tbody>
-        </table>
+          <div className="text-2xl w-full flex justify-center gap-2 md:justify-start md:w-fit">
+            <span>Produto:</span>
+            <strong>{product.name}</strong>
+          </div>
+
+          <Button className="text-2xl w-full md:w-auto mt-auto" size="lg" variant="outline" asChild>
+            <Link href="/">
+              Voltar ao catálogo
+            </Link>
+          </Button>
+        </div>
       </div>
-
-      <Button className="w-full max-w-xs text-2xl" size={"lg"} variant="outline" asChild>
-        <Link href={'/'}>
-          Voltar ao catálogo
-        </Link>
-      </Button>
     </main>
   )
 }
