50560e1517c14022077dca265827b04c8920d167
CarlosLonghi <carlos.longhi@digitalone.com.br>
2024-07-23 16:50:21 -0300

    feat: add Metadata noindex to page success

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -3,6 +3,12 @@ import { stripe } from "@/lib/stripe";
 import Image from "next/image";
 import Link from "next/link";
 import { redirect } from 'next/navigation';
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Checkout | Guitar Shop",
+  robots: 'noindex, nofollow',
+};
 
 interface Product {
   name: string;
